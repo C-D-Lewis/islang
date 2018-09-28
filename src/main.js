4a1733e49260e49401ad863a3d6cd77f580cf96f
C-D-Lewis <Bonsitm@gmail.com>
2018-09-28 22:16:07 +0100

    Testable transformations

--- src/main.js
+++ src/main.js
@@ -18,17 +18,94 @@ const getIndentLevel = (line) => {
   return numSpaces / 2;
 };
 
+/**
+ * Transform a line of is into a line of JS
+ * @param {String} input - The entire input line.
+ * @param {String[]} tokens - The tokens of the line split on ' '.
+ * @returns {String} The line of JS.
+ */
+const transform = (input, tokens) => {
+  // Empty line
+  if (!input.length) {
+    return '';
+  }
+
+  // Comment
+  if (input.startsWith('//')) {
+    return input;
+  }
+
+  // Variable declaration
+  if (tokens[0] === 'value') {
+    if (tokens[2] !== 'is') {
+      throw new Error('value statement should include \'is\' to initialise it with a value');
+    }
+
+    const varValue = tokens[3];
+    return `let ${tokens[1]} = ${varValue};`;
+  }
+
+  // Variable assignment
+  if (tokens[1] === 'is') {
+    const varValue = tokens.slice(2).join(' ');
+    return `${tokens[0]} = ${varValue};`;
+  }
+
+  // Until loop
+  if (tokens[0] === 'until') {
+    if (tokens[2] !== 'equals') {
+      throw new Error('until statement must specify limit with \'equals\'');
+    }
+
+    return `while(${tokens[1]} !== ${tokens[3]}) {`;
+  }
+
+  // Invoke function
+  if (tokens[0] === 'run') {
+    const functionArgs = tokens.slice(3);
+    return `${tokens[1]}(${functionArgs.join(',')});`;
+  }
+
+  // Log statement
+  if (tokens[0] === 'log') {
+    // Literal or value?
+    if (!input.includes('\'')) {
+      throw new Error('log must always be passed a string, which can include template values with { and }');
+    }
+
+    const strStart = input.indexOf('\'') + 1;
+    const strEnd = input.indexOf('\'', strStart);
+    let logStr = input.substring(strStart, strEnd).split('{').join('${');
+    return `console.log(\`${logStr}\`);`;
+  }
+
+  // Function declaration start
+  // TODO: Make gets optional for simple functions
+  if (tokens[0] === 'task') {
+    if (!tokens.includes('gets')) {
+      throw new Error('task statement should include arguments after task name using \'gets\'');
+    }
+
+    const functionArgs = tokens.slice(3).filter(item => item !== 'nothing');
+    return `function ${tokens[1]}(${functionArgs.join(',')}) {`;
+  }
+
+  // Function declaration end
+  if (tokens[0] === 'end') {
+    return '}';
+  }
+
+  // Return statement
+  if (tokens[0] === 'return') {
+    return `return ${tokens.slice(1).join(' ')};`;
+  }
+};
+
 const processLines = (lines) => {
   let output = '// compiled from islang source\n\n';
   lines.forEach((line) => {
     const indentLevel = getIndentLevel(line);
     const input = line.trim();
-
-    // Empty line
-    if (!input.length) {
-      output += '\n';
-      return;
-    }
 
     // Optionally annotate with source line
     output += ' '.repeat(indentLevel * 2);
@@ -37,82 +114,7 @@ const processLines = (lines) => {
     }
     const tokens = input.split(' ');
     
-    // Comment
-    if (input.startsWith('//')) {
-      return;
-    }
-
-    // Variable declaration
-    if (input.startsWith('value ')) {
-      if (!tokens.includes('is')) {
-        throw new Error('value statement should include \'is\' to initialise it with a value');
-      }
-
-      const varValue = tokens[3];
-      output += `let ${tokens[1]} = ${varValue};\n`;
-      return;
-    }
-
-    // Variable assignment
-    if (input.includes(' is ')) {
-      const varValue = tokens.slice(2).join(' ');
-      output += `${tokens[0]} = ${varValue};\n`;
-      return;
-    }
-
-    // Until loop
-    if (input.startsWith('until ')) {
-      if (!tokens.includes('equals')) {
-        throw new Error('until statement must specify limit with \'equals\'');
-      }
-
-      output += `while(${tokens[1]} !== ${tokens[3]}) {\n`;
-      return;
-    }
-
-    // Invoke function
-    if (input.startsWith('run ')) {
-      const functionArgs = tokens.slice(3);
-      output += `${tokens[1]}(${functionArgs.join(',')});\n`;
-      return;
-    }
-
-    // Log statement
-    if (input.startsWith('log ')) {
-      // Literal or value?
-      if (!input.includes('\'')) {
-        throw new Error('log must always be passed a string, which can include template values with { and }');
-      }
-
-      const strStart = input.indexOf('\'') + 1;
-      const strEnd = input.indexOf('\'', strStart);
-      let logStr = input.substring(strStart, strEnd).split('{').join('${');
-      output += `console.log(\`${logStr}\`);\n`;
-      return;
-    }
-
-    // Function declaration start
-    if (input.startsWith('task ')) {
-      if (!tokens.includes('gets')) {
-        throw new Error('task statement should include arguments after task name using \'gets\'');
-      }
-
-      const functionArgs = tokens.slice(3).filter(item => item !== 'nothing');
-      output += `function ${tokens[1]}(${functionArgs.join(',')}) {\n`;
-      return;
-    }
-
-    // Function declaration end
-    if (input === 'end') {
-      output += '}\n';
-      return;
-    }
-
-    // Return statement
-    if (input.startsWith('return')) {
-      output += `return ${tokens.slice(1).join(' ')};\n`;
-      return;
-    }
+    output += transform(input, tokens) + '\n';
   });
 
   return output;
@@ -135,3 +137,4 @@ const main = () => {
 };
 
 main();
+
